dc976859c9a35d1e3a1786819a2e27506fbd3383
shinigami <aitelfatmiy@gmail.com>
2026-07-16 02:11:22 +0100

    Update admin credentials in config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 
 const IMKLI_CONFIG = {
-  ADMIN_USERNAME: "admin",
-  ADMIN_PASSWORD: "ayoub123",
+  ADMIN_USERNAME: "IMKLI1913",
+  ADMIN_PASSWORD: "IMKLI1913",
   SUPABASE_URL: "https://xaeoenxloyhxqnqklzlr.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhZW9lbnhsb3loeHFucWtsemxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3ODY5NDcsImV4cCI6MjA5OTM2Mjk0N30.WRcSeYgbj85PdMoma0l5TDCozA0jCIOtuboBC0_npvo",
   SUPABASE_STORAGE_BUCKET: "meal-images",
